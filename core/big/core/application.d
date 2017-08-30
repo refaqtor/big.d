37f62e9c19faac37591b68ba7c8d29d8d6210a43
Kozhevnikov Peter <kozhevnikov@myriad-corp.com>
2017-08-30 13:09:24 +0300

    Make DI in Application (#9) (#10)
    * Add Application singleton
    
    * Add Application singleton

---
+++ core/big/core/application.d
@@ -0,0 +1,107 @@
+/**
+* Copyright: 2017 © LLC CERERIS
+* License: MIT
+* Authors: LLC CERERIS
+*/
+
+module big.core.application;
+
+import dich.container: Container;
+import dich.provider: InstanceProvider;
+
+/// Return Application instance (syntactic sugar).
+Application app()
+{
+  return Application.get();
+}
+
+/// Application instance (Singleton)
+final class Application
+{
+  public:
+    /** This method gives a Application instance.
+    * Based on Singleton example.
+    *
+    * Returns:
+    *   $(D Application) instance
+    */
+    static Application get()
+    {
+      if(!_isInstance)
+      {
+        synchronized(Application.classinfo)
+        {
+          if(!_instance)
+          {
+            _instance = new Application();
+          }
+
+          _isInstance = true;
+        }
+      }
+
+      return _instance;
+    }
+
+    /** Register service for Dependency injection. 
+    *
+    * Params:
+    *   instance = Instance of service.
+    *   name = Name of registered service.
+    *
+    * Returns:
+    *   $(D bool) result of registration. Return $(D false) if service alredy registered.
+    */
+    bool register(T)(T instance, string name = "")
+    {
+      bool status;
+
+      try
+      {
+        auto provider = new InstanceProvider(instance);
+        _container.register!(T)(provider, name);
+        status = true;
+      }
+      catch(Exception e)
+      {}
+
+      return status;
+    }
+
+    /** Get service by type. 
+    *
+    * Params:
+    *   T = Type of service.
+    *
+    * Returns:
+    *   Result service by type.
+    */
+    T get(T)()
+    {
+      return _container.get!T();
+    }
+
+    /** Get service by type and name. 
+    *
+    * Params:
+    *   T = Type of service.
+    *   name = Name of service.
+    *
+    * Returns:
+    *   Result service by type and name.
+    */
+    T get(T)(string name)
+    {
+      return _container.get!T(name);
+    }
+
+  private:
+    this()
+    {
+      _container = new Container();
+    }
+
+    static bool _isInstance;
+    __gshared Application _instance;
+    __gshared Container _container;
+}
---
+++ test/big-d/core/applicationtest.d
@@ -0,0 +1,56 @@
+/**
+* Copyright: 2017 © LLC CERERIS
+* License: MIT
+* Authors: LLC CERERIS
+*/
+
+module test.big.core.applicationtest;
+
+import big.core.application: app; 
+
+import checkit.assertion;
+import checkit.bdd;
+
+class TestService{}
+
+unittest
+{
+  scenario!("Register service", ["application"])
+  ({
+    given!"Service instance"
+    ({
+      auto service = new TestService();
+
+      when!"Register service instance without name"
+      ({
+        app().register(service).shouldBeTrue();
+
+        then!"Success get Test instance"
+        ({
+          app().get!TestService().shouldBeInstanceOf!TestService();
+        });
+      });
+
+      when!"Register service instance by name"
+      ({
+        app().register(service, "test").shouldBeTrue();
+
+        then!"Success get Test instance"
+        ({
+          app().get!TestService("test").shouldBeInstanceOf!TestService();
+        });
+      });
+
+      when!"Register duplicate service instance by name"
+      ({
+        app().register(service, "test1").shouldBeTrue();
+        app().register(service, "test1").shouldBeFalse();
+
+        then!"Success get Test instance"
+        ({
+          app().get!TestService("test1").shouldBeInstanceOf!TestService();
+        });
+      });
+    });
+  });
+}
