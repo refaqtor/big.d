9cc320b3d0aa9ab49313814c00eeb7a776eff547
Nikita Eliseev <eliseev@myriad-corp.com>
2017-09-08 11:09:48 +0300

    temp

--- router/big/router/routerservice.d
+++ router/big/router/routerservice.d
@@ -16,7 +16,7 @@ import std.regex: matchFirst;
 struct Middleware
 {
   string middlewareName;
-  Composite delegate(Composite) middlewareHandler;
+  void delegate(ref Composite) middlewareHandler;
 }
 
 struct Target
@@ -55,13 +55,68 @@ class RouterService
         source = sourceAttribute.getValue().get!string();
       }
       
+      auto typeAttribute = data.get!Attribute("type");
+      string type;
+      if(typeAttribute is null)
+      {
+        bigLog.warning("Data without type can't be routed!");
+        return;
+      }
+      else
+      {
+        type = typeAttribute.getValue().get!string();
+      }
+      
       foreach(Rout rout; _routs)
       {
-//        auto  = matchFirst("a = 42;", regex(`(?P<var>\w+)\s*=\s*(?P<value>\d+);`));
+        string sourceRegex = rout.source;
+        auto sourceMatchFirstValue = matchFirst(source, sourceRegex);
+        
+        string typeRegex = rout.type;
+        auto typeMatchFirstValue = matchFirst(type, typeRegex);
+        
+        if(sourceMatchFirstValue.length == 0 || typeMatchFirstValue.length == 0)
+        {
+          return;
+        }
+        
+        string middlewareRegex = rout.middleware;
+        Composite middlewareComposite = data;
+        if(!(middlewareRegex is null))
+        {
+          foreach(Middleware middleware; _middlewares)
+          {
+            auto middlewareMatchFirstValue = matchFirst(middleware.middlewareName, middlewareRegex);
+            if(middlewareMatchFirstValue.length != 0)
+            {
+              bigLog.trace("Sending data '" ~ middlewareComposite.toString ~ "' to middleware with name '" ~
+                middleware.middlewareName ~ "'");
+              middleware.middlewareHandler(middlewareComposite);
+              if(middlewareComposite is null)
+              {
+                bigLog.warning("Data has not passed the middleware check with the name '" ~
+                  middleware.middlewareName ~ "'! Data will not be sent to any targets");
+                return;
+              }
+              bigLog.trace("Middleware check wiht name '" ~ middleware.middlewareName ~ "' it's ok!");
+            }
+          }
+          bigLog.trace("All middleware checks were successful!");
+        }
+        
+        string targetRegex = rout.target;
+        foreach(Target target; _targets)
+        {
+          auto targetMatchFirstValue = matchFirst(target.targetName, targetRegex);
+          if(targetMatchFirstValue.length != 0)
+          {
+            target.targetHandler(middlewareComposite);
+          }
+        }
       }
     }
 
-    void addMiddleware(string middlewareName, Composite delegate(Composite) middlewareHandler)
+    void addMiddleware(string middlewareName, void delegate(ref Composite) middlewareHandler)
     {
       Middleware newMiddleware;
       newMiddleware.middlewareName = middlewareName;
@@ -81,7 +136,8 @@ class RouterService
     {
       if(canFind(_routs, newRout))
       {
-        bigLog.warning("Several equivalent routs were found in the configuration files!" ~
+        bigLog.warning("Attempt to insert a router with equivalent parameters was detected!." ~
+          "Please, check your configuration files or code were you insert the routers. " ~
           "Routs duplicates will be ignored!");
       }
       else
