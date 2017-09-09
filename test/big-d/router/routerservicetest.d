78aad0afa1bbc18bd71609fb5d18d60279166594
Nikita Eliseev <eliseev@myriad-corp.com>
2017-09-09 14:33:24 +0300

    temp

--- test/big-d/router/routerservicetest.d
+++ test/big-d/router/routerservicetest.d
@@ -22,12 +22,6 @@ import checkit;
 //
 //}
 
-void everySourceAndTypeHandler(Composite composite)
-{
-  import std.stdio;
-  writeln(composite.get!Attribute("source").getValue());
-}
-
 unittest
 {
   scenario!("RouterService should rout data correctly", ["router"])
@@ -36,32 +30,115 @@ unittest
     ({       
       RouterService router = new RouterService();
       
-      Rout rout = Rout(r"\bsource\d*\b", r"\btype\d*\b", r"everySourceAndTypeTarget");
+      Rout rout = Rout(r"\bsource\d*\b", r"\btype\d*\b", r"\bcustomTarget\b");
+      Rout copyRout = Rout(r"\bsource\d*\b", r"\btype\d*\b", r"\bcustomTarget\b");
+      router.addRout(rout);
+      router.addRout(copyRout);
+      
+      int expectedCount = 0; //shows how many times the function was called
+      void customTarget(Composite composite)
+      {
+        expectedCount++;
+      }
+      
+      router.addTarget("customTarget", toDelegate(&(customTarget)));
+      
+      Rout[] expected = [];
+      expected ~= Rout(r"\bsource\d*\b", r"\btype\d*\b", r"\bcustomTarget\b");
+      router.getRouts.shouldEqual(expected);
+      
+      when!"Some data is created and routed"
+      ({
+          Composite data1 = new Composite("source1");
+          data1.add(new Attribute("source", "source1"));
+          data1.add(new Attribute("type", "type1"));
+          data1.add(new Attribute("target", "customTarget"));
+          
+          Composite data2 = new Composite("source2");
+          data2.add(new Attribute("source", "source2"));
+          data2.add(new Attribute("type", "type2"));
+          data2.add(new Attribute("target", "customTarget"));
+          
+          Composite data3 = new Composite("source3");
+          data3.add(new Attribute("type", "type2"));
+          data3.add(new Attribute("target", "customTarget"));
+          
+          Composite data4 = new Composite("source4");
+          data4.add(new Attribute("source", "source4"));
+          data4.add(new Attribute("target", "customTarget"));
+          
+          Composite data5 = new Composite("source5");
+          data5.add(new Attribute("source", "sourceName5"));
+          data5.add(new Attribute("type", "type2"));
+          data5.add(new Attribute("target", "customTarget"));
+          
+          Composite data6 = new Composite("source6");
+          data6.add(new Attribute("source", "source6"));
+          data6.add(new Attribute("type", "typeName6"));
+          data6.add(new Attribute("target", "customTarget"));
+          
+          router.process(data1);
+          router.process(data2);
+          router.process(data3);
+          router.process(data4);
+          router.process(data5);
+          router.process(data6);
+          
+        then!"!These data correctly routed"
+        ({
+            expectedCount.shouldEqual(2);
+        });
+      });
+    });
+  });
+  
+  scenario!("RouterService should rout data correctly", ["router"])
+  ({
+    given!"Configured router service with route which have middleware"
+    ({       
+      RouterService router = new RouterService();
+      
+      Rout rout = Rout(r"\bsource\d*\b", r"\bDATA\b|\bPOST\b", r"\bcustomTarget\b", r"\bmiddleware\d*\b");
       router.addRout(rout);
       
-      router.addTarget("everySourceAndTypeTarget", toDelegate(&(everySourceAndTypeHandler)));
+      int expectedCount = 0; //shows how many times the function was called
+      void customTarget(Composite composite)
+      {
+        expectedCount++;
+      }
+      router.addTarget("customTarget", toDelegate(&(customTarget)));
       
-      when!"Create RouterServiceInitManager"
+      Rout[] expected = [];
+      expected ~= Rout(r"\bsource\d*\b", r"\bDATA\b|\bPOST\b", r"\bcustomTarget\b", r"\bmiddleware\d*\b");
+      router.getRouts.shouldEqual(expected);
+      
+      void customMiddleware1(ref Composite composite)
+      {
+        if(composite.get!Attribute("type").getValue() != "DATA")
+        {
+          composite = null;
+        }
+      }
+      router.addMiddleware("middleware1", toDelegate(&(customMiddleware1)));
+      
+      when!"Some data is created and routed"
       ({
-        then!"!!!!!!!!!!"
+          Composite data1 = new Composite("source1");
+          data1.add(new Attribute("source", "source1"));
+          data1.add(new Attribute("type", "DATA"));
+          data1.add(new Attribute("target", "customTarget"));
+          
+          Composite data2 = new Composite("source2");
+          data2.add(new Attribute("source", "source2"));
+          data2.add(new Attribute("type", "POST"));
+          data2.add(new Attribute("target", "customTarget"));
+          
+          router.process(data1);
+          router.process(data2);
+          
+        then!"!These data correctly routed"
         ({
-            Rout[] expected = [];
-            expected ~= Rout(r"\bsource\d*\b", r"\btype\d*\b", r"everySourceAndTypeTarget");
-            
-            router.getRouts.shouldEqual(expected);
-            
-            Composite data1 = new Composite("source11");
-            data1.add(new Attribute("source", "source1"));
-            data1.add(new Attribute("type", "type1"));
-            data1.add(new Attribute("target", "everySourceAndTypeTarget"));
-            
-            Composite data2 = new Composite("source22");
-            data2.add(new Attribute("source", "source2"));
-            data2.add(new Attribute("type", "type2"));
-            data2.add(new Attribute("target", "everySourceAndTypeTarget"));
-            
-            router.process(data1);
-            router.process(data2);
+            expectedCount.shouldEqual(1);
         });
       });
     });
